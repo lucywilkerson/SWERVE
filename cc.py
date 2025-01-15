b5ad14e80a639aecf42badf2cf119864890ccdab
Lucy Wilkerson <lucy.a.wilkerson@gmail.com>
2025-01-15 12:37:04 -0500

    Update cc.py

--- cc.py
+++ cc.py
@@ -4,6 +4,7 @@ import pickle
 import numpy as np
 import pandas as pd
 import sys
+from geopy.distance import geodesic
 
 
 data_dir = os.path.join('..', '2024-AGU-data')
@@ -20,7 +21,22 @@ filtered_df.reset_index(drop=True, inplace=True)
 
 sites = filtered_df['site_id'].tolist()
 #sites = ['10052', '10238', '10255'] # For testing
-print("site_1\tsite_2\tcc")
+print("site_1\tsite_2\tcc\tdist(km)")
+
+#function to read TVA or NERC data to make below loop less complicated
+def read_TVA_or_NERC (df,site_id):
+  if df['data_source'][idx_1] == 'NERC':
+      #reading in data for site if NERC
+      fname = os.path.join(data_dir, 'processed', site_id, 'GIC_measured_NERC.pkl')
+  elif df['data_source'][idx_1] == 'TVA':
+      #reading in data for site if TVA
+      site_id = "".join(site_id.split()) #removing space from name to match file name
+      fname = os.path.join(data_dir, 'processed', site_id, 'GIC_measured_TVA.pkl')
+  with open(fname, 'rb') as f:
+      #print(f"Reading {fname}")
+      site_data = pickle.load(f)
+  site_df = pd.DataFrame(site_data)
+  return site_df
 
 
 rows = []
@@ -28,57 +44,29 @@ for idx_1, site_1 in enumerate(sites):
   # Only comparing measured GIC for now
   if filtered_df['data_type'][idx_1] != 'GIC' or filtered_df['data_class'][idx_1] != 'measured':
     continue
-  elif filtered_df['data_source'][idx_1] == 'NERC':
-    #reading in data for site_1 if NERC
-    fname = os.path.join(data_dir, 'processed', site_1, 'GIC_measured_NERC.pkl')
-    with open(fname, 'rb') as f:
-      #print(f"Reading {fname}")
-      site_1_data = pickle.load(f)
-    site_1_df = pd.DataFrame(site_1_data)
   else:
-    #reading in data for site_1 if TVA
-    site_1 = "".join(site_1.split()) #removing space from name to match file name
-    fname = os.path.join(data_dir, 'processed', site_1, 'GIC_measured_TVA.pkl')
-    with open(fname, 'rb') as f:
-      #print(f"Reading {fname}")
-      site_1_data = pickle.load(f)
-    site_1_df = pd.DataFrame(site_1_data)
+    site_1_df = read_TVA_or_NERC(filtered_df,site_1)
   for idx_2, site_2 in enumerate(sites):
     if idx_1 <= idx_2:  # Avoid duplicate pairs
       continue
     # Only comparing measured GIC for now
     elif filtered_df['data_type'][idx_2] != 'GIC' or filtered_df['data_class'][idx_2] != 'measured':
       continue
-    elif filtered_df['data_source'][idx_2] == 'NERC':
-      #reading in data for site_2 if NERC
-      fname = os.path.join(data_dir, 'processed', site_2, 'GIC_measured_NERC.pkl')
-      with open(fname, 'rb') as f:
-        #print(f"Reading {fname}")
-        site_2_data = pickle.load(f)
-      site_2_df = pd.DataFrame(site_2_data)
+    else: 
+      site_2_df = read_TVA_or_NERC(filtered_df,site_2)
       cc = np.corrcoef(site_1_df['modified'][0]['data'], site_2_df['modified'][0]['data'])[0, 1]
-      #issue w cc calculation, need arrays to have same size 
+      #issue w cc calculation, get rid of nan values
+      #finding distange between sites in km
+      dist = geodesic((filtered_df['geo_lat'][idx_1], filtered_df['geo_lon'][idx_1]), 
+                      (filtered_df['geo_lat'][idx_2], filtered_df['geo_lon'][idx_2])).km
+      print(f"{site_1}\t{site_2}\t{cc}\t{dist}")
+      rows.append([site_1, site_2, cc, dist])
 
-      print(f"{site_1}\t{site_2}\t{cc}")
-      rows.append([site_1, site_2, cc])
-    else:
-      #reading in data for site_2 if TVA
-      site_2 = "".join(site_2.split()) #removing space from name to match file name
-      fname = os.path.join(data_dir, 'processed', site_2, 'GIC_measured_TVA.pkl')
-      with open(fname, 'rb') as f:
-        #print(f"Reading {fname}")
-        site_2_data = pickle.load(f)
-      site_2_df = pd.DataFrame(site_2_data)
-      cc = np.corrcoef(site_1_df['modified'][0]['data'], site_2_df['modified'][0]['data'])[0, 1]
-
-      print(f"{site_1}\t{site_2}\t{cc}")
-      rows.append([site_1, site_2, cc])
 
 
 # Print the results again in order of decreasing correlation coefficient
-rows = np.array(rows)
-sorted_rows = rows[np.argsort(rows[:, 2])[::-1]]
-print("site_1\tsite_2\tcc")
+rows_df = pd.DataFrame(rows, columns=['site_1', 'site_2', 'cc', 'dist(km)'])
+sorted_rows = rows_df.sort_values(by='cc', ascending=False)
 np.set_printoptions(threshold=sys.maxsize)
 print(sorted_rows)
 
