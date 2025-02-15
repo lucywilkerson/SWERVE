8a90a69b2f8a96b2e2a6ee2216d6ac92d1db62ce
Lucy Wilkerson <lucy.a.wilkerson@gmail.com>
2025-02-15 02:42:39 -0500

    Update with beta

--- cc.py
+++ cc.py
@@ -26,7 +26,7 @@ info_df.reset_index(drop=True, inplace=True)
 sites = info_df['site_id'].tolist()
 #sites = ['10052', '10207'] # For testing
 
-columns = ['site_1', 'site_2', 'cc', 'dist(km)', 'bad_1', 'bad_2', 'std_1', 'std_2']
+columns = ['site_1', 'site_2', 'cc', 'dist(km)', 'bad_1', 'bad_2', 'std_1', 'std_2', 'beta_diff']
 print('\t'.join(columns))
 
 def write_table(rows, out_dir):
@@ -115,8 +115,11 @@ for idx_1, row in info_df.iterrows():
     # Compute distance between sites in km
     distance = site_distance(info_df, idx_1, idx_2)
 
-    print(f"{site_1_id}\t{site_2_id}\t{cc:+.2f}\t{distance:6.1f}\t\t{bad_1}\t{bad_2}\t{std_1:.2f}\t{std_2:.2f}")
-    rows.append([site_1_id, site_2_id, cc, distance, bad_1, bad_2, std_1, std_2])
+    # Compute difference in beta
+    dbeta = info_df['interpolated_beta'][idx_1] - info_df['interpolated_beta'][idx_2]
+
+    print(f"{site_1_id}\t{site_2_id}\t{cc:+.2f}\t{distance:6.1f}\t\t{bad_1}\t{bad_2}\t{std_1:.2f}\t{std_2:.2f}\t{dbeta:.2f}")
+    rows.append([site_1_id, site_2_id, cc, distance, bad_1, bad_2, std_1, std_2, dbeta])
 
     # TODO:add a column in the printout of # mins
 
--- plot_scatter.py
+++ plot_scatter.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import pandas as pd
 import numpy as np
 
 import matplotlib.pyplot as plt
@@ -18,7 +19,7 @@ def savefig(fname):
 
   print(f"Writing {fname}.png")
   plt.savefig(f'{fname}.png', dpi=600, bbox_inches="tight")
-
+"""
 plt.scatter(df['dist(km)'], np.abs(df['cc']))
 plt.xlabel('Distance (km)')
 plt.ylabel('|cc|')
@@ -33,7 +34,59 @@ plt.ylabel('|cc|')
 plt.grid(True)
 savefig(os.path.join(results_dir, 'cc_vs_std_scatter'))
 plt.close()
+"""
+plt.scatter(np.abs(df['beta_diff']), np.abs(df['cc']))
+plt.xlabel(r'|$\Delta \log_{10} (\beta)$|')
+plt.ylabel('|cc|')
+plt.grid(True)
+savefig(os.path.join(results_dir, 'cc_vs_beta_scatter'))
+plt.show()
+plt.close()
 
-# plot_maps
-# plot_scatter
-# plot_timeseries
+
+####################################################################
+# Site scatter
+
+def site_plots(info_df, cc_df):
+
+    def plot_cc(site_id, cc_df, distance=True):
+        cc = []
+        dist = []
+        avg_std = []
+        for idx, row in cc_df.iterrows():
+            if row['site_1'] == site_id:
+                site_2_id = row['site_2']
+            elif row['site_2'] == site_id:
+                site_2_id = row['site_1']
+            else:
+                continue
+            cc.append(row['cc'])
+            dist.append(row['dist(km)'])
+            avg_std.append(np.mean([row['std_1'], row['std_2']]))
+        if distance == True:
+            plt.scatter(dist, np.abs(cc))
+            plt.xlabel('Distance (km)')
+        else:
+            plt.scatter(avg_std, np.abs(cc))
+            plt.xlabel('Average standard deviation (A)')
+        plt.ylabel('|cc|')
+        plt.ylim(0, 1)
+        plt.title(site_id)
+        plt.grid(True)
+
+
+    # Plotting maps and cc plots for each site
+    for idx_1, row in info_df.iterrows():
+        site_1_id = row['site_id']
+        if site_1_id not in sites:
+            continue
+
+        # plotting cc vs distance
+        plot_cc(site_1_id, cc_df)
+        savefig(site_1_id, 'cc_vs_dist_scatter')
+        plt.close()
+
+        # plotting cc vs standard deviation
+        plot_cc(site_1_id, cc_df, distance=False)
+        savefig(site_1_id, 'cc_vs_std_scatter')
+        plt.close()
