833494c6093021ca3ddc227acd06872a59c47bc5
Lucy Wilkerson <lucy.a.wilkerson@gmail.com>
2025-03-24 15:47:21 -0400

    add scatter plot

--- find_low_cc.py
+++ find_low_cc.py
@@ -3,6 +3,7 @@ import pandas as pd
 import pickle
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 data_dir = os.path.join('..', '2024-AGU-data')
@@ -54,3 +55,77 @@ def site_filt(info_df, cc_df, cc_lim):
 
 cc_max = 0.3
 site_filt(info_df, cc_df, cc_max)
+
+#################################################################################################
+# defining avg |cc| for each site
+for idx_1, row in info_df.iterrows():
+    site_1_id = row['site_id']
+    site_cc = []
+    for idx_2, row in cc_df.iterrows():
+        if row['site_1'] == site_1_id:
+            site_2_id = row['site_2']
+        elif row['site_2'] == site_1_id:
+            site_2_id = row['site_1']
+        else:
+            continue
+        cc = np.abs(row['cc'])
+        site_cc.append(cc)
+    avg_cc = np.mean(site_cc)
+    info_df.loc[info_df['site_id'] == site_1_id, 'avg_cc'] = avg_cc #adding mean cc to info_df
+
+# Adding min(avg_cc) to cc_df
+for idx, row in cc_df.iterrows():
+    site_1_id = row['site_1']
+    site_2_id = row['site_2']
+    avg_cc_1 = info_df.loc[info_df['site_id'] == site_1_id, 'avg_cc'].values[0]
+    avg_cc_2 = info_df.loc[info_df['site_id'] == site_2_id, 'avg_cc'].values[0]
+    cc_df.loc[idx, 'min_avg_cc'] = min(avg_cc_1, avg_cc_2)
+
+# Making scatter plot! (code from plot_scatter.py)
+results_dir = os.path.join('..', '2024-AGU-data', '_results')
+fmts = ['png', 'pdf']
+def savefig(fdir, fname, fmts=fmts):
+    if not os.path.exists(fdir):
+        os.makedirs(fdir)
+    fname = os.path.join(fdir, fname)
+
+    for fmt in fmts:
+        print(f"    Saving {fname}.{fmt}")
+        if fmt == 'png':
+            plt.savefig(f'{fname}.{fmt}', dpi=600, bbox_inches='tight')
+        else:
+            plt.savefig(f'{fname}.{fmt}', bbox_inches='tight')
+
+def scatter_with_colorbar(df, color_col, cbar_label, plot_title, file_name):
+    # Define 10 discrete color bins for the color column
+    bins = np.linspace(np.abs(df[color_col]).min(), np.abs(df[color_col]).max(), 10)
+    norm = plt.Normalize(bins.min(), bins.max())
+    cmap = plt.cm.get_cmap('viridis', len(bins) - 1)
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    sm.set_array([])
+
+    # Plotting scatter with colorbar
+    fig, ax = plt.subplots(figsize=(12, 5))
+    sc = ax.scatter(df['dist(km)'], np.abs(df['cc']), c=np.abs(df[color_col]), cmap=cmap, norm=norm)
+    ax.set_xlabel('Distance [km]')
+    ax.set_ylabel('|cc|')
+    #ax.set_title(plot_title)
+    ax.grid(True)
+    
+    # Set up colorbar
+    cax = fig.add_axes([0.92, 0.1, 0.02, 0.8])  # Position for the colorbar
+    cbar = plt.colorbar(sm, cax=cax, ticks=bins, label=cbar_label)
+    
+    # Add dots to the colorbar
+    #cbar.ax.clear()
+    #for i, b in enumerate(bins[:-1]):
+        #cax.plot([0.5], [b], 'o', color=cmap(i), markersize=5, transform=cax.get_yaxis_transform(), clip_on=False)
+    cbar.set_ticks(bins)
+    cbar.set_ticklabels([f'{b:.2f}' for b in bins])
+    cbar.ax.yaxis.set_label_position('right')
+    cbar.set_label(cbar_label)
+    cbar.ax.xaxis.set_visible(False)
+    savefig(results_dir, file_name)
+    plt.close()
+
+scatter_with_colorbar(cc_df, 'min_avg_cc', r'min mean |cc|', 'CC vs Distance with Min |cc| Colorbar', 'cc_vs_dist_vs_min_scatter')
