5fa8dbd215c77863868b29d2503c3fdea8888f37
Lucy Wilkerson <lucy.a.wilkerson@gmail.com>
2025-05-14 11:57:46 -0400

    add cross correlation analysis

--- cc.py
+++ cc.py
@@ -27,7 +27,7 @@ info_df.reset_index(drop=True, inplace=True)
 sites = info_df['site_id'].tolist()
 #sites = ['10052', '10207', 'Bull Run'] # For testing
 
-columns = ['site_1', 'site_2', 'cc', 'dist(km)', 'bad_1', 'bad_2', 'std_1', 'std_2', 'beta_diff', 'volt_diff(kV)', 'lat_diff', 'power_pool_1','power_pool_2','region_1','region_2']
+columns = ['site_1', 'site_2', 'cc', 'dist(km)', 'bad_1', 'bad_2', 'std_1', 'std_2', 'beta_diff', 'volt_diff(kV)', 'lat_diff', 'power_pool_1','power_pool_2','region_1','region_2','peak_xcorr','peak_xcorr_lag(min)']
 print('\t'.join(columns))
 
 def write_table(rows, rows_md, out_dir):
@@ -164,8 +164,18 @@ for idx_1, row in info_df.iterrows():
     # finding power pool/region comparison
     pool, reg = power_pool_filt(pool_1, pool_2, reg_1, reg_2)
 
-    print(f"{site_1_id}\t{site_2_id}\t{cc:+.2f}\t{distance:6.1f}\t\t{bad_1}\t{bad_2}\t{std_1:.2f}\t{std_2:.2f}\t{dbeta:.2f}\t{dvolt:.2f}\t{dlat:.2f}\t{pool_1}\t{pool_2}\t{reg_1}\t{reg_2}")
-    rows.append([site_1_id, site_2_id, cc, distance, bad_1, bad_2, std_1, std_2, dbeta, dvolt, dlat, pool_1, pool_2, reg_1, reg_2])
+    # finding peak cross correlation
+    if cc < 0:
+      site_1_data = -site_1_data
+    lags = np.arange(-60, 61, 1) 
+    cross_corr = [np.corrcoef(site_1_data[~np.isnan(site_1_data) & ~np.isnan(np.roll(site_2_data, lag))], 
+                            np.roll(site_2_data, lag)[~np.isnan(site_1_data) & ~np.isnan(np.roll(site_2_data, lag))])[0, 1] for lag in lags]
+    # Find the maximum absolute cross-correlation and corresponding lag
+    max_xcorr = max(cross_corr)
+    max_lag = lags[cross_corr.index(max_xcorr)]
+
+    print(f"{site_1_id}\t{site_2_id}\t{cc:+.2f}\t{distance:6.1f}\t\t{bad_1}\t{bad_2}\t{std_1:.2f}\t{std_2:.2f}\t{dbeta:.2f}\t{dvolt:.2f}\t{dlat:.2f}\t{pool_1}\t{pool_2}\t{reg_1}\t{reg_2}\t{max_xcorr:.2f}\t{max_lag}")
+    rows.append([site_1_id, site_2_id, cc, distance, bad_1, bad_2, std_1, std_2, dbeta, dvolt, dlat, pool_1, pool_2, reg_1, reg_2, max_xcorr, max_lag])
 
     # Format rows as Markdown
     site_1_id_x = site_1_id.lower().replace(' ','')
@@ -176,7 +186,7 @@ for idx_1, row in info_df.iterrows():
     site_1_id_link = f'[{site_1_id_x}](../../../tree/main/_processed/{site_1_id_x})'
     site_2_id_link = f'[{site_2_id_x}](../../../tree/main/_processed/{site_2_id_x})'
 
-    rows_md.append([site_1_id_link, site_2_id_link, cc_link, distance, bad_1, bad_2, std_1, std_2, dbeta, dvolt, dlat, pool_1, pool_2, reg_1, reg_2])
+    rows_md.append([site_1_id_link, site_2_id_link, cc_link, distance, bad_1, bad_2, std_1, std_2, dbeta, dvolt, dlat, pool_1, pool_2, reg_1, reg_2, max_xcorr, max_lag])
 
     # TODO:add a column in the printout of # mins
 
