7a006ef33471a6497acdfdda368853e759ddc3f0
Bob Weigel <Robert.S.Weigel@gmail.com>
2025-05-20 12:36:31 -0400

    plot_imf

--- plot_imf.py
+++ plot_imf.py
@@ -5,104 +5,59 @@ from datetime import datetime, timedelta
 
 from datetick import datetick
 
+from storminator import DATA_DIR, LOG_DIR, plt_config, savefig, savefig_paper, subset
+
+import utilrsw
+logger = utilrsw.logger(log_dir=LOG_DIR)
+
 import matplotlib.pyplot as plt
 
-plt.rcParams['font.family'] = 'Times New Roman'
-plt.rcParams['mathtext.fontset'] = 'cm'
-plt.rcParams['xtick.labelsize'] = 14
-plt.rcParams['ytick.labelsize'] = 14
-plt.rcParams['axes.labelsize'] = 16
-plt.rcParams['figure.dpi'] = 100
-plt.rcParams['savefig.dpi'] = 600
 
-data_dir = os.path.join('..', '2024-May-Storm-data')
-base_dir = os.path.join(data_dir, '_processed')
-paper_dir = os.path.join('..','2024-May-Storm-paper')
+def read():
+  file_path = os.path.join(DATA_DIR, 'mage', 'bcwind.h5')
+  logger.info(f'Reading {file_path}')
+  h5file = h5py.File(file_path,'r')
 
-def subset(time, data, start, stop):
-  idx = np.logical_and(time >= start, time <= stop)
-  if data.ndim == 1:
-    return time[idx], data[idx]
-  return time[idx], data[idx,:]
+  data = {}
+  for key in h5file.keys():
+    logger.info(f'  Found {key}')
+    data[key] = h5file[key][()]
 
-start = datetime(2024, 5, 10, 15, 0)
-stop = datetime(2024, 5, 12, 6, 0)
+  data['time'] = np.array([datetime(1858, 11, 17) + timedelta(days=day) for day in data['MJD']])
 
-def savefig(sid, fname, sub_dir="", fmts=['png','pdf']):
-  fdir = os.path.join(base_dir, sid.lower().replace(' ', ''), sub_dir)
-  if not os.path.exists(fdir):
-    os.makedirs(fdir)
-  fname = os.path.join(fdir, fname)
+  for i in range(data['al'].size):
+    if data['time'][i] <= datetime(2024, 5, 10, 0, 0):
+      data['al'][i] = np.nan
+      data['al'][i] = np.nan
+  return data
 
-  for fmt in fmts:
-    print(f"    Saving {fname}.{fmt}")
-    plt.savefig(f'{fname}.{fmt}', bbox_inches='tight')
+data = read()
 
-def savefig_paper(fname, sub_dir="", fmts=['png','pdf']):
-  fdir = os.path.join(paper_dir, sub_dir)
-  if not os.path.exists(fdir):
-    os.makedirs(fdir)
-  fname = os.path.join(fdir, fname)
+limits = plt_config()
 
-  for fmt in fmts:
-    print(f"    Saving {fname}.{fmt}")
-    plt.savefig(f'{fname}.{fmt}', bbox_inches='tight')
-
-
-# Open file
-file_path = os.path.join(data_dir, 'mage', 'bcwind.h5')
-print(f'Reading {file_path}')
-imf_data = h5py.File(file_path,'r')
-"""data_keys = imf_data.keys()
-print(data_keys)
-exit()"""
-
-# Assigning variables
-al = imf_data['al'][()]
-ae = imf_data['ae'][()]
-Kp = imf_data['Kp'][()]
-symh = imf_data['symh'][()]
-temp = imf_data['Temp'][()]
-mach = imf_data['Magnetosonic Mach'][()]
-Vx = imf_data['Vx'][()]
-Bx = imf_data['Bx'][()]
-By = imf_data['By'][()]
-Bz = imf_data['Bz'][()]
-
-# Convert time from MJD
-mjd = imf_data['MJD'][()]
-time_original = np.array([datetime(1858, 11, 17) + timedelta(days=day) for day in mjd])
-time = time_original
-
-#fig, axes = plt.subplots(7, 1, figsize=(8.5, 11))
 plt.figure(figsize=(8.5, 11))
 gs = plt.gcf().add_gridspec(7, 1)
 axes = gs.subplots(sharex=True)
 
 # Plotting al and ae
-
-for i in range(al.size):
-  if time_original[i] <= datetime(2024, 5, 10, 0, 0):
-    al[i] = np.nan
-    ae[i] = np.nan
-
-time, al = subset(time_original, al, start, stop)
+time, al = subset(data['time'], data['al'], limits['data'][0], limits['data'][1])
 axes[0].plot(time,-al, label=r'$-$AL', color='k', linewidth=1)
-time, ae = subset(time_original, ae, start, stop)
+time, ae = subset(data['time'], data['ae'], limits['data'][0], limits['data'][1])
 axes[0].plot(time,ae, label='AE', color='m', linewidth=0.5)
 axes[0].set_ylabel('[nT]')
 axes[0].legend(ncol=2)
 
 # Plotting Kp
+# TODO: Should be able to do this without the loop (using only plt.stairs or plt.step).
 kp_times = []
 kp_values = []
-# Creating a 1-hour grid from start to stop
-current_time = start
-while current_time <= stop:
+# Creating a 1-hour grid from limits['data'][0] to limits['data'][1]
+current_time = limits['data'][0]
+while current_time <= limits['data'][1]:
   # Finding the index of the closest Kp value (Kp is every 3 hours, centered at :30)
   # Find the previous 3-hour interval
   kp_idx = None
-  for i, t in enumerate(time_original):
+  for i, t in enumerate(data['time']):
     if t.hour % 3 == 0 and t.minute == 0:
       kp_time_center = t + timedelta(hours=1.5)
       # If current_time falls within this 3-hour interval, use this Kp
@@ -111,12 +66,9 @@ while current_time <= stop:
         break
   if kp_idx is not None:
     kp_times.append(current_time)
-    kp_values.append(Kp[kp_idx])
+    kp_values.append(data['Kp'][kp_idx])
   current_time += timedelta(hours=1)
 
-#kp_times = np.array(kp_times)
-#kp_values = np.array(kp_values)
-#kp_times, kp_values = subset(kp_times, kp_values, start, stop)
 axes[1].step(kp_times, kp_values, where='post', color='k')
 axes[1].fill_between(kp_times, kp_values, 0, step='post', color='k')
 axes[1].set_ylabel(r'K$_p$')
@@ -124,37 +76,34 @@ axes[1].yaxis.set_major_locator(plt.MaxNLocator(integer=True))
 axes[1].set_ylim(4.5, 9.5)
 
 # Plotting symh
-time, symh = subset(time_original, symh, start, stop)
+time, symh = subset(data['time'], data['symh'], limits['data'][0], limits['data'][1])
 axes[2].plot(time, symh, color='k', linewidth=0.8)
 axes[2].set_ylabel('SYM-H [nT]')
 
 # Plotting temperature
-time, temp = subset(time_original, temp, start, stop)
+time, temp = subset(data['time'], data['Temp'], limits['data'][0], limits['data'][1])
 axes[3].plot(time, temp / 1e6, color='k', linewidth=0.8)
 axes[3].set_ylabel(r'T [MK]')
 
 # Plotting mach
-time, mach = subset(time_original, mach, start, stop)
+time, mach = subset(data['time'], data['Magnetosonic Mach'], limits['data'][0], limits['data'][1])
 axes[4].plot(time, mach, color='k', linewidth=0.8)
 axes[4].set_ylabel(r'Mach')
 
 # Plotting Vx
-time, Vx = subset(time_original, Vx, start, stop)
+time, Vx = subset(data['time'], data['Vx'], limits['data'][0], limits['data'][1])
 axes[5].plot(time, Vx/1000, color='k', linewidth=0.8)  # divide by 1000 to get in km/s
 axes[5].set_ylabel(r'V$_x$ [km/s]')
 
 # Plotting Bx, By, Bz IMF
-#time, Bx = subset(time_original, Bx, start, stop)
-#axes[6].plot(time, Bx, label=r'B$_x^\text{IMF}$', linewidth=0.5)
-time, By = subset(time_original, By, start, stop)
+time, By = subset(data['time'], data['By'], limits['data'][0], limits['data'][1])
 axes[6].plot(time, By, label=r'B$_y^\text{IMF}$', color='k', linewidth=0.5)
-time, Bz = subset(time_original, Bz, start, stop)
+time, Bz = subset(data['time'], data['Bz'], limits['data'][0], limits['data'][1])
 axes[6].plot(time, Bz, label=r'B$_z^\text{IMF}$', color='m', linewidth=0.5)
 axes[6].set_ylabel('[nT]')
 axes[6].legend(loc='upper right', ncol=2)
 
-xlims = [datetime(2024, 5, 10, 11, 0), stop]
-#plt.gca().set_xlim(datetime.datetime(2024, 5, 10, 11, 0), stop)
+xlims = [limits['xlims'][0], limits['xlims'][1]]
 for ax in axes:
   ax.set_xlim(xlims)
   ax.grid(True)
@@ -162,7 +111,7 @@ for ax in axes:
   if ax != axes[1]:
     ax.minorticks_on()
     ax.grid(which='minor', axis='both', linestyle=':', linewidth=0.5)
-  
+
   leg = ax.get_legend()
   if leg is not None:
     # change the line width for the legend
@@ -174,6 +123,5 @@ for ax in axes[:-1]:
   ax.set_xticklabels([])
 datetick('x', axes=axes[-1])
 
-savefig('_imf','imf_mage')
-savefig_paper('imf_mage')
-#plt.show()
+savefig('_imf', 'imf_mage', logger)
+savefig_paper('imf_mage', logger)
