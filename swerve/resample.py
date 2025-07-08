f6fe78c5911c52c32890f7080fcb809947efd6e2
Bob Weigel <Robert.S.Weigel@gmail.com>
2025-07-08 17:15:08 -0400

    do not drop if non-uniform cadence

--- swerve/resample.py
+++ swerve/resample.py
@@ -1,4 +1,4 @@
-def resample(time, data, start, stop, ave=None, logger=None, logger_indent=0):
+def resample(time, data, start, stop, ave=None, inclusive='both', logger=None, logger_indent=0):
 
   def print_info0():
     if logger is None:
@@ -52,18 +52,20 @@ def resample(time, data, start, stop, ave=None, logger=None, logger_indent=0):
     time = numpy.array(time)
   if not numpy.issubdtype(time.dtype, numpy.datetime64):
     time = time.astype('datetime64[ns]')
+
   dts_uniq = cadence(time, data, logger=logger, logger_indent=logger_indent)
-  if len(dts_uniq) > 1:
-    raise ValueError(f"Time steps are not uniform. Unique dts [s]: {dts_uniq}")
-  dts_uniq = dts_uniq[0]  # Unique time difference in nanoseconds.
+  #if len(dts_uniq) > 1:
+  #  raise ValueError(f"Time steps are not uniform. Unique dts [s]: {dts_uniq/1e9}")
+  if logger:
+    logger.info(f"{logger_indent}  Warning: Time steps are not uniform. Unique dts [s]: {dts_uniq/1e9}")
+  dts_uniq = dts_uniq[0]  # Smallest time difference in nanoseconds.
 
   time, data = subset(time, data, start, stop)
   print_info(time, data, "After subsetting")
 
-  # inclusive='left' excludes the stop time.
+  # inclusive='left' includes start and excludes the stop time.
   freq = f"{int(dts_uniq)}ns"  # Frequency in nanoseconds.
-  date_range = pandas.date_range(start=start, end=stop, freq=freq, inclusive='left')
-  #date_range = pandas.date_range(start=start, end=stop, freq=freq)
+  date_range = pandas.date_range(start=start, end=stop, freq=freq, inclusive=inclusive)
   datetime_series = pandas.Series(time)
   dfo = pandas.DataFrame(data, index=datetime_series)
 
@@ -97,70 +99,3 @@ def resample(time, data, start, stop, ave=None, logger=None, logger_indent=0):
     print_info(time, data, "After resampling")
 
   return time, data
-
-
-def resample_test():
-  start   = datetime.datetime(2024, 5, 10, 0, 0, 0)
-  stop    = datetime.datetime(2024, 5, 10, 0, 1, 0)
-
-  # Create list of 1-second timestamps from start to stop (exclusive of stop)
-  time = [start + datetime.timedelta(seconds=i) for i in range(int((stop - start).total_seconds()))]
-  time = numpy.array(time)
-  data = numpy.ones((len(time), 1))
-  data = -1 + numpy.cumsum(data, axis=0)
-
-  logger.info(40 * '-')
-  logger.info("Testing trimming start")
-  start_x = datetime.datetime(2024, 5, 10, 0, 0, 10)
-  time_, data_ = resample(time, data, start_x, stop, ave=None, logger=logger)
-  assert time_[0] == start_x, f"Expected time[0] = {start_x}, got {time_[0]}"
-  assert time_[-1] == stop, f"Expected time[-1] = {stop}, got {time_[-1]}"
-
-  logger.info(40 * '-')
-  logger.info("Testing trimming stop")
-  stop_x = datetime.datetime(2024, 5, 10, 0, 0, 20)
-  time_, data_ = resample(time, data, start_x, stop_x, ave=None, logger=logger)
-  assert time_[0] == start_x, f"Expected time[0] = {start_x}, got {time_[0]}"
-  assert time_[-1] == stop_x, f"Expected time[-1] = {stop_x}, got {time_[-1]}"
-
-  logger.info(40 * '-')
-  logger.info("Testing padding start")
-  start_x = datetime.datetime(2024, 5, 9, 23, 59, 10)
-  time_, data_ = resample(time, data, start_x, stop, ave=None, logger=logger)
-  assert time_[0] == start_x, f"Expected time[0] = {start_x}, got {time_[0]}"
-  assert time_[-1] == stop, f"Expected time[-1] = {stop}, got {time_[-1]}"
-
-  logger.info(40 * '-')
-  logger.info("Testing padding stop")
-  stop_x = datetime.datetime(2024, 5, 10, 0, 1, 20)
-  time_, data_ = resample(time, data, start, stop_x, ave=None, logger=logger)
-  assert time_[0] == start, f"Expected time[0] = {start}, got {time_[0]}"
-  assert time_[-1] == stop_x, f"Expected time[-1] = {stop}, got {time_[-1]}"
-
-  logger.info(40 * '-')
-  logger.info("Testing averaging using ave in nanoseconds")
-  start_x = datetime.datetime(2024, 5, 10, 0, 0, 10)
-  stop_x = datetime.datetime(2024, 5, 10, 0, 0, 29)
-  ave = 10
-  time_, data_ = resample(time, data, start_x, stop_x, ave=ave*1e9, logger=logger)
-  to = start_x + datetime.timedelta(seconds=ave/2 - 0.5)
-  tf = stop_x - datetime.timedelta(seconds=ave/2 - 0.5)
-  assert time_[0] == to, f"Expected time[0] = {to}, got {time_[0]}"
-  assert data_[0] == 14.5, f"Expected time[0] = {to}, got {time_[0]}"
-  assert time_[-1] == tf, f"Expected time[-1] = {tf}, got {time_[-1]}"
-  assert data_[-1] == 24.5, f"Expected time[0] = {to}, got {time_[0]}"
-
-  logger.info("Testing averaging using ave as string")
-  time_, data_ = resample(time, data, start_x, stop_x, ave='10s', logger=logger)
-  to = start_x + datetime.timedelta(seconds=ave/2 - 0.5)
-  tf = stop_x - datetime.timedelta(seconds=ave/2 - 0.5)
-  assert time_[0] == to, f"Expected time[0] = {to}, got {time_[0]}"
-  assert data_[0] == 14.5, f"Expected time[0] = {to}, got {time_[0]}"
-  assert time_[-1] == tf, f"Expected time[-1] = {tf}, got {time_[-1]}"
-  assert data_[-1] == 24.5, f"Expected time[0] = {to}, got {time_[0]}"
-
-  #print(d)
-
-
-if __name__ == '__main__':
-  resample_test()
