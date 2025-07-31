253c1a229b0818be4c8193d0f5909d9554ebaece
Lucy Wilkerson <lucy.a.wilkerson@gmail.com>
2025-07-31 17:25:33 -0400

    generalize metrics.py

--- metrics_.py
+++ metrics_.py
@@ -43,35 +43,41 @@ info_dict, data_all = read(all_file)
 
 TVA_sites = ['Bull Run', 'Montgomery', 'Union', 'Widows Creek']
 
-def write_metrics_table(info_dict, data_all, columns, data_type=None):
-    def mean_exclude_invalid(series):
+def write_metrics_table(info_dict, data_all, column_names, data_type=None):
+    def mean_exclude_invalid(series): #excludes nan_fill values from mean calculation
         value = pd.to_numeric(series)
         valid = value[value != nan_fill]
         return np.mean(valid) if len(valid) > 0 else ''
+    
+    def nan_remove(s): #removes nan_fill values
+        print(s)
+        return '' if s == nan_fill else s
 
+    columns = list(column_names.keys()) #names of columns
     rows = []
     for sid in info_dict.keys():
         if data_type in info_dict[sid].keys():
             data_classes = info_dict[sid][data_type].keys()
             if 'measured' in data_classes and 'calculated' in data_classes:
+                #reading in measured data
                 time_meas = data_all[sid][data_type]['measured'][0]['modified']['time']
                 data_meas = data_all[sid][data_type]['measured'][0]['modified']['data']
                 time_meas, data_meas = subset(time_meas, data_meas, start, stop)
                 if data_type == 'B':
-                    data_meas = np.linalg.norm(data_meas, axis=1)
+                    data_meas = np.linalg.norm(data_meas, axis=1) #taking norm of B to get B_H
                     data_meas_original = data_meas
 
-                fill = (len(columns)-1)*[nan_fill]
-                row = [sid, *fill]
+                fill = (len(columns)-1)*[nan_fill] 
+                row = [sid, *fill] #setting row to fill w values
                 # Adding std meas
                 row[1] = f"{np.nanstd(data_meas):.1f}"
-                column_adjust = 2 if data_type == 'GIC' else 3
+                column_adjust = 2 if data_type == 'GIC' else 3 #column adjust defined by number of models
                 for idx, data_source in enumerate(info_dict[sid][data_type]['calculated']):
                     if data_type == 'GIC' and data_source not in ['GMU', 'TVA']:
                         continue
                     if data_type == 'B' and data_source not in ['SWMF', 'MAGE', 'OpenGGCM']:
                         continue
-                    
+                    #reading in calculated data
                     time_calc = data_all[sid][data_type]['calculated'][idx]['original']['time']
                     if data_type == 'GIC':  
                         if data_source == 'GMU':
@@ -90,11 +96,9 @@ def write_metrics_table(info_dict, data_all, columns, data_type=None):
                         data_calc = data_all[sid][data_type]['calculated'][idx]['original']['data']
                         time_calc, data_calc = subset(time_calc, data_calc, start, stop)
                         data_calc = np.linalg.norm(data_calc[:,0:2], axis=1)
-
-                        # Interpolate measured data
+                        # Interpolate measured B data
                         time_meas_ts = [time.mktime(t.timetuple()) for t in time_meas]
                         time_calc_ts = np.array([time.mktime(t.timetuple()) for t in time_calc])
-                        # Interpolate measured data
                         data_meas = np.interp(time_calc_ts, time_meas_ts, data_meas_original)
                         idx_adjust = idx
                     
@@ -103,24 +107,24 @@ def write_metrics_table(info_dict, data_all, columns, data_type=None):
                     cc = np.corrcoef(data_meas[valid], data_calc[valid])[0,1]
                     numer = np.sum((data_meas[valid]-data_calc[valid])**2)
                     denom = np.sum((data_meas[valid]-data_meas[valid].mean())**2)
-
+                    # Calcualte std
                     row[2+idx_adjust] = f"{np.nanstd(data_calc):.1f}"
-
+                    # calculate cc**2 and pe
                     if np.sum(valid) > 1:
                         row[2+column_adjust+idx_adjust] = cc**2
                         row[2+(2*column_adjust)+idx_adjust] = 1 - numer / denom
                         for i in [2+column_adjust+idx_adjust, 2+(2*column_adjust)+idx_adjust]:
                             if np.isnan(row[i]):
-                                row[i] = nan_fill
+                                row[i] = nan_fill #fill with nan_fill if cc is nan
                             else:
                                 row[i] = f"{row[i]:.2f}"
-                    else:
+                    else: #fill with nan_fill if not enough valid data
                         row[2+column_adjust+idx_adjust] = nan_fill
                         row[2+(2*column_adjust)+idx_adjust] = nan_fill
                 rows.append(row)
-    df = pd.DataFrame(rows, columns=columns)
+    df = pd.DataFrame(rows, columns=columns) #create dataframe
     logger.info(df)
-
+    # Add row of mean values
     mean_row = {'site_id': 'Mean'}
     for col in df.columns:
         if col != 'site_id':
@@ -129,40 +133,36 @@ def write_metrics_table(info_dict, data_all, columns, data_type=None):
             else:
                 mean_row[col] = f"{mean_exclude_invalid(df[col]):.2f}"
     df.loc[len(df)] = mean_row
+    #rename columns with LaTex
+    df = df.rename(columns=column_names)
+    # Format and save df as .md and .tex files
+    fname = os.path.join(DATA_DIR, "_results", f"{data_type.lower()}_table")
+    formatters = {col: nan_remove for col in df.columns}
+    print(f"Writing GIC prediction comparison tables to {fname}.{{md,tex}}")
+    # Apply nan_remove to each cell before writing to markdown
+    df_md = gic_df.applymap(nan_remove)
+    df_md.to_markdown(fname + ".md", index=False, floatfmt=".2f")
+    df.to_latex(fname + ".tex", formatters=formatters, index=False, escape=False)
 
     return df
 
 
-columns = ['site_id', 'sigma_data', 'sigma_tva', 'sigma_gmu','cc_tva', 'cc_gmu', 'pe_tva', 'pe_gmu']
-gic_df = write_metrics_table(info_dict, data_all, columns, data_type='GIC')
+# Define mapping from original columns to renamed columns
+gic_column_names = {
+    'site_id': 'Site ID',
+    'sigma_data': r'$\sigma$ [A]',
+    'sigma_tva': r'$\sigma_\text{TVA}$',
+    'sigma_gmu': r'$\sigma_\text{Ref}$',
+    'cc_tva': r'$\text{cc}^2_\text{TVA}$',
+    'cc_gmu': r'$\text{cc}^2_\text{Ref}$',
+    'pe_tva': r'$\text{pe}_\text{TVA}$',
+    'pe_gmu': r'$\text{pe}_\text{Ref}$'
+}
 
-gic_df = gic_df.rename(columns={'site_id':'Site ID',
-            'sigma_data':r'$\sigma$ [A]',
-            'sigma_tva':r'$\sigma_\text{TVA}$',
-            'sigma_gmu':r'$\sigma_\text{Ref}$',
-            'cc_tva':r'$\text{cc}^2_\text{TVA}$',
-            'cc_gmu':r'$\text{cc}^2_\text{Ref}$',
-            'pe_tva':r'$\text{pe}_\text{TVA}$',
-            'pe_gmu':r'$\text{pe}_\text{Ref}$'
-            }
-        )
-
-fname = os.path.join(DATA_DIR, "_results", "gic_table")
-def nan_remove(s):
-    print(s)
-    return '' if s == nan_fill else s
-formatters = {col: nan_remove for col in gic_df.columns}
-print(f"Writing GIC prediction comparison tables to {fname}.{{md,tex}}")
-# Apply nan_remove to each cell before writing to markdown
-gic_df_md = gic_df.applymap(nan_remove)
-gic_df_md.to_markdown(fname + ".md", index=False, floatfmt=".2f")
-gic_df.to_latex(fname + ".tex", formatters=formatters, index=False, escape=False)
+gic_df = write_metrics_table(info_dict, data_all, gic_column_names, data_type='GIC')
 
 
-columns = ['site_id', 'sigma_data', 'sigma_swmf', 'sigma_mage', 'sigma_ggcm', 'cc_swmf', 'cc_mage', 'cc_ggcm', 'pe_swmf', 'pe_mage', 'pe_ggcm']
-b_df = write_metrics_table(info_dict, data_all, columns, data_type='B')
-
-b_df = b_df.rename(columns={'site_id':'Site ID',
+b_column_names = {'site_id':'Site ID',
             'sigma_data':r'$\sigma$ [nT]',
             'sigma_swmf':r'$\sigma_\text{SWMF}$',
             'sigma_mage':r'$\sigma_\text{MAGE}$',
@@ -174,13 +174,5 @@ b_df = b_df.rename(columns={'site_id':'Site ID',
             'pe_mage':r'$\text{pe}_\text{MAGE}$',
             'pe_ggcm':r'$\text{pe}_\text{GGCM}$'
             }
-        )
 
-fname = os.path.join(DATA_DIR, "_results", "b_table")
-formatters = {col: nan_remove for col in b_df.columns}
-print(f"Writing GIC prediction comparison tables to {fname}.{{md,tex}}")
-# Apply nan_remove to each cell before writing to markdown
-b_df_md = b_df.applymap(nan_remove)
-b_df_md.to_markdown(fname + ".md", index=False, floatfmt=".2f")
-b_df.to_latex(fname + ".tex", formatters=formatters, index=False, escape=False)
-                    
+b_df = write_metrics_table(info_dict, data_all, b_column_names, data_type='B')                   
