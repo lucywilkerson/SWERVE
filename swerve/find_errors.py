f6bfa659ecdb8bd074ea2c6aa6c67ab9e8d4fb5e
Lucy Wilkerson <lucy.a.wilkerson@gmail.com>
2025-11-03 11:22:04 -0500

    Update find_errors.py

--- swerve/find_errors.py
+++ swerve/find_errors.py
@@ -4,7 +4,7 @@ import datetime
 # This function will read the raw GIC data and determine if there is an error with the timeseries. If there is, it will log the error and output it
 # will be added to info.py and run before metrics are calculated
 
-def find_errors(sid, logger=None, data_type='GIC', low_signal_threshold=3.5, baseline_buffer=1, spike_threshold = 40, std_limit=5, max_cadence=60, max_gap=600):
+def find_errors(sid, logger=None, data_type='GIC', low_signal_threshold=3.5, baseline_buffer=1, spike_threshold = 40, std_limit=15, max_cadence=60, max_gap=600):
     """low_signal_threshold, baseline_buffer, spike_threshold, and std_limit in [A]
        max_cadence, max_gap, and max_constant in [s]"""
     from swerve import config, site_read, cadence
