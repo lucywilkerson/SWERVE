eb7fb453defb59e0cf6198edb54c6cdc06b2e759
Lucy Wilkerson <lucy.a.wilkerson@gmail.com>
2026-09-10 01:18:49 -0400

    update to handle multiple input events

--- swerve/write_info_csv.py
+++ swerve/write_info_csv.py
@@ -1,9 +1,13 @@
 
 # read in data sources, data types, data class from config
 # then, according to that, read in site name
-# ideally, also site latitude and longitude
 # add manual error for no lat/lon - maybe make this an option? (do this in add_row func)
 # also manual error for no file found... hopefully not common
+
+from requests_cache import logger
+
+from info import CONFIG
+
 
 def write_info_csv():
     """
@@ -31,54 +35,33 @@ def write_info_csv():
     data_types = CONFIG['info_kwargs']['data_type']
     data_classes = CONFIG['info_kwargs']['data_class']
     data_sources = CONFIG['info_kwargs']['data_source']
-    # TODO: add events so can make info file of multiple events; for now, just one
-    # also will need warning if source does not have data for given event
-    event = CONFIG['event']
+    events = CONFIG['event']
 
     data_dir = CONFIG['dirs']['original']
 
     # empty list to hold info
     info_list = []
-    
-    for data_source in data_sources:
-        if data_source == 'NERC':
-            # read site names for NERC measured GIC/B and save to csv
-            if 'measured' in data_classes:
-                data_class = 'measured'
-                for data_type in data_types:
-                    if data_type == 'GIC':
-                        file_dir = os.path.join(data_dir, f'{data_source.lower()}', event, f'{data_type.lower()}')
-                        file = os.path.join(file_dir, 'gic_monitors.csv')
-                    elif data_type == 'B':
-                        file_dir = os.path.join(data_dir, f'{data_source.lower()}', event, 'mag')
-                        file = os.path.join(file_dir, 'magnetometers.csv')
-                    else:
-                        raise ValueError(f"Data type not recognized: {data_type}. Valid options are 'GIC' and 'B'.")
-                    logger.info(f"    Reading {file}")
-                    if not os.path.exists(file):
-                        raise FileNotFoundError(f"File not found: {file}")
-                    with open(file, 'r') as csvfile:
-                        rows = csv.reader(csvfile, delimiter=',')
-                        # skip header
-                        next(rows)
-                        for row in rows:
-                            site_id = row[0]
-                            geo_lat = float(row[1])
-                            geo_lon = -float(row[2])
-                            info_list = _add_info_row(info_list, site_id, geo_lat, geo_lon, data_type, data_class, data_source, event)
-            # no calculated data provided by NERC, skip
-            if 'calculated' in data_classes:
-                logger.info(f'   No calculated data for data source {data_source}; skipping.')
+    for event in events:
+        for data_source in data_sources:
+            # Check that data is available for given event and data source, if not skip to next event/data source
+            event_exists = _check_event(event, data_source, logger)
+            if not event_exists:
+                continue
 
-        if data_source == 'TVA':
-            # read site names for TVA measured GIC/B and save to csv
-            if 'measured' in data_classes:
-                data_class = 'measured'
-                for data_type in data_types:
-                    # read site names for NERC measured GIC/B and save to csv
-                    if data_type == 'GIC':
-                        file_dir = os.path.join(data_dir, f'{data_source.lower()}', event, f'{data_type.lower()}', 'GIC-measured')
-                        file = os.path.join(file_dir, 'GIC_monitors.dat')
+            if data_source == 'NERC':
+                # read site names for NERC measured GIC/B and save to csv
+                if 'measured' in data_classes:
+                    data_class = 'measured'
+                    for data_type in data_types:
+                        if data_type == 'GIC':
+                            file_dir = os.path.join(data_dir, f'{data_source.lower()}', event, f'{data_type.lower()}')
+                            file = os.path.join(file_dir, 'gic_monitors.csv')
+                        elif data_type == 'B':
+                            file_dir = os.path.join(data_dir, f'{data_source.lower()}', event, 'mag')
+                            file = os.path.join(file_dir, 'magnetometers.csv')
+                        else:
+                            raise ValueError(f"Data type not recognized: {data_type}. Valid options are 'GIC' and 'B'.")
+                        logger.info(f"    Reading {file}")
                         if not os.path.exists(file):
                             raise FileNotFoundError(f"File not found: {file}")
                         with open(file, 'r') as csvfile:
@@ -87,58 +70,64 @@ def write_info_csv():
                             next(rows)
                             for row in rows:
                                 site_id = row[0]
-                                geo_lat = float(row[2])
-                                geo_lon = float(row[3])
-                                info_list = _add_info_row(info_list, site_id, geo_lat, geo_lon, data_type, data_class, data_source, event)
-                    elif data_type == 'B':
-                        file_dir = os.path.join(data_dir, f'{data_source.lower()}', event, 'mag')
-                        file = os.path.join(file_dir, 'TVAmagmetadata.dat')
-                        if not os.path.exists(file):
-                            raise FileNotFoundError(f"File not found: {file}")
-                        with open(file, 'r') as csvfile:
-                            rows = csv.reader(csvfile, delimiter=',')
-                            for row in rows:
-                                site_id = row[0]
                                 geo_lat = float(row[1])
-                                geo_lon = float(row[2])
+                                geo_lon = -float(row[2])
                                 info_list = _add_info_row(info_list, site_id, geo_lat, geo_lon, data_type, data_class, data_source, event)
-                    else:
-                        raise ValueError(f"Data type not recognized: {data_type}. Valid options are 'GIC' and 'B'.")
-            # TVA only has calculated GIC, not B. Add calculated if GIC, skip if B.                  
-            if 'calculated' in data_classes:
-                data_class = 'calculated'
-                if data_type == 'GIC':
-                    file_dir = os.path.join(data_dir, f'{data_source.lower()}', event, f'{data_type.lower()}', 'GIC-calculated')
-                    # TODO: no nice file with listed names of calculated GIC sites for TVA!
-                else:
-                    logger.info(f'   No calculated {data_type} data for data source {data_source}; skipping.')
-    
-        if data_source == 'Parry2025':
-            # read site names for CAN data from Parry 2025 paper
-            for data_type in data_types:
-                if 'measured' in data_classes:
-                    data_class = 'measured'
-                    if data_type == 'GIC':
-                        file_dir = os.path.join(data_dir, f'{data_source.lower()}', event, f'{data_type.lower()}')
-                        file = os.path.join(file_dir, 'parry_2025_info.csv')
-                    with open(file, 'r') as csvfile:
-                        rows = csv.reader(csvfile, delimiter=',')
-                        # skip header
-                        next(rows)
-                        for row in rows:
-                            site_id = row[0]
-                            geo_lat = float(row[1])
-                            geo_lon = float(row[2])
-                            info_list = _add_info_row(info_list, site_id, geo_lat, geo_lon, data_type, data_class, data_source, event)
+                # no calculated data provided by NERC, skip
+                if 'calculated' in data_classes:
+                    logger.info(f'   No calculated data for data source {data_source}; skipping.')
 
-        if data_source == 'Parry2024':
-            # read site names for CAN data from Parry 2024 paper
-            for data_type in data_types:
+            if data_source == 'TVA':
+                # read site names for TVA measured GIC/B and save to csv
                 if 'measured' in data_classes:
                     data_class = 'measured'
+                    for data_type in data_types:
+                        # read site names for NERC measured GIC/B and save to csv
+                        if data_type == 'GIC':
+                            file_dir = os.path.join(data_dir, f'{data_source.lower()}', event, f'{data_type.lower()}', 'GIC-measured')
+                            file = os.path.join(file_dir, 'GIC_monitors.dat')
+                            if not os.path.exists(file):
+                                raise FileNotFoundError(f"File not found: {file}")
+                            with open(file, 'r') as csvfile:
+                                rows = csv.reader(csvfile, delimiter=',')
+                                # skip header
+                                next(rows)
+                                for row in rows:
+                                    site_id = row[0]
+                                    geo_lat = float(row[2])
+                                    geo_lon = float(row[3])
+                                    info_list = _add_info_row(info_list, site_id, geo_lat, geo_lon, data_type, data_class, data_source, event)
+                        elif data_type == 'B':
+                            file_dir = os.path.join(data_dir, f'{data_source.lower()}', event, 'mag')
+                            file = os.path.join(file_dir, 'TVAmagmetadata.dat')
+                            if not os.path.exists(file):
+                                raise FileNotFoundError(f"File not found: {file}")
+                            with open(file, 'r') as csvfile:
+                                rows = csv.reader(csvfile, delimiter=',')
+                                for row in rows:
+                                    site_id = row[0]
+                                    geo_lat = float(row[1])
+                                    geo_lon = float(row[2])
+                                    info_list = _add_info_row(info_list, site_id, geo_lat, geo_lon, data_type, data_class, data_source, event)
+                        else:
+                            raise ValueError(f"Data type not recognized: {data_type}. Valid options are 'GIC' and 'B'.")
+                # TVA only has calculated GIC, not B. Add calculated if GIC, skip if B.                  
+                if 'calculated' in data_classes:
+                    data_class = 'calculated'
                     if data_type == 'GIC':
-                        file_dir = os.path.join(data_dir, f'{data_source.lower()}', event, f'{data_type.lower()}')
-                        file = os.path.join(file_dir, 'parry_2024_info.csv')
+                        file_dir = os.path.join(data_dir, f'{data_source.lower()}', event, f'{data_type.lower()}', 'GIC-calculated')
+                        # TODO: no nice file with listed names of calculated GIC sites for TVA!
+                    else:
+                        logger.info(f'   No calculated {data_type} data for data source {data_source}; skipping.')
+        
+            if data_source == 'Parry2025':
+                # read site names for CAN data from Parry 2025 paper
+                for data_type in data_types:
+                    if 'measured' in data_classes:
+                        data_class = 'measured'
+                        if data_type == 'GIC':
+                            file_dir = os.path.join(data_dir, f'{data_source.lower()}', event, f'{data_type.lower()}')
+                            file = os.path.join(file_dir, 'parry_2025_info.csv')
                         with open(file, 'r') as csvfile:
                             rows = csv.reader(csvfile, delimiter=',')
                             # skip header
@@ -148,6 +137,29 @@ def write_info_csv():
                                 geo_lat = float(row[1])
                                 geo_lon = float(row[2])
                                 info_list = _add_info_row(info_list, site_id, geo_lat, geo_lon, data_type, data_class, data_source, event)
+
+            if data_source == 'Parry2024':
+                # read site names for CAN data from Parry 2024 paper
+                for data_type in data_types:
+                    if 'measured' in data_classes:
+                        data_class = 'measured'
+                        if data_type == 'GIC' or data_type == 'DMM':
+                            file_dir = os.path.join(data_dir, f'{data_source.lower()}', event)
+                            file = os.path.join(file_dir, 'parry_2024_info.csv')
+                            with open(file, 'r') as csvfile:
+                                rows = csv.reader(csvfile, delimiter=',')
+                                # skip header
+                                next(rows)
+                                for row in rows:
+                                    if (data_type == 'DMM' and row[3] == 'DMM') or (data_type == 'GIC' and row[3] == 'GIC'):
+                                        site_id = row[0]
+                                        geo_lat = float(row[1])
+                                        geo_lon = float(row[2])
+                                        info_list = _add_info_row(info_list, site_id, geo_lat, geo_lon, data_type, data_class, data_source, event)
+
+            if data_source == 'Zhang2020':
+                print('no info yet...')
+                exit()
 
     # compile list into one df
     info_df = pd.DataFrame(info_list)
@@ -171,4 +183,13 @@ def _add_info_row(info_list, site_id, geo_lat, geo_lon, data_type, data_class, d
                     'manual_error': manual_error})
     return info_list
 
-
+def _check_event(event,data_source,logger):
+    # checks if events are valid for all data sources, if not logger warning
+    import os
+    event_folder = os.path.join(CONFIG['dirs']['original'], f'{data_source.lower()}', event)
+    if not os.path.exists(event_folder):
+        logger.warning(f"   Event {event} not found for data source {data_source}. Skipping...")
+        return False
+    else:
+        logger.info(f"   Adding sites for event {event} from data source {data_source} to info.csv.")
+        return True
